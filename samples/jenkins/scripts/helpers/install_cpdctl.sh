76a3d06864933ebdb4a10ef9957310d4eacde768
Jaime Viniegra <jviniegra@jaimes-mbp.mad.es.ibm.com>
2022-04-25 13:05:27 +0200

    changed the repo

--- samples/jenkins/scripts/helpers/install_cpdctl.sh
+++ samples/jenkins/scripts/helpers/install_cpdctl.sh
@@ -1,9 +1,9 @@
 
 curl -s https://api.github.com/repos/IBM/cpdctl/releases | \
-  jq -r '.[0].assets[] | select (.name == "cpdctl_linux_amd64.tar.gz") | .url'  | \
-  xargs -I {} curl -sSL -H 'Accept: application/octet-stream' "{}" -o cpdctl_linux_amd64.tar.gz
+  jq -r '.[0].assets[] | select (.name == "cpdctl_darwin_amd64.tar.gz") | .url'  | \
+  xargs -I {} curl -sSL -H 'Accept: application/octet-stream' "{}" -o cpdctl_darwin_amd64.tar.gz
 
-tar -xvf cpdctl_linux_amd64.tar.gz
+tar -xvf cpdctl_darwin_amd64.tar.gz
 
 chmod a+rw $CPDCONFIG
 
